11fe2be54618c92f1392ecba2156a0860b01b0df
Ross Fletcher <rossfletcher108@gmail.com>
2025-07-09 16:35:21 +0100

    fix

--- src/auth.js
+++ src/auth.js
@@ -5,7 +5,12 @@ import { db } from "./db";
 
 export const { auth, handlers, signOut, signIn } = NextAuth({
   adapter: PostgresAdapter(db),
-  providers: [GitHub],
+  providers: [
+    GitHubProvider({
+      clientId: process.env.AUTH_GITHUB_ID,
+      clientSecret: process.env.AUTH_GITHUB_SECRET,
+    }),
+  ],
   trustHost: true,
   callbacks: {
     session: async ({ session, user }) => {
